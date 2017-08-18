46a6847b4b0ce3d0755888c9d6a2d6d0d8b9921b
Noel Figuera <luisnoelf@gmail.com>
2017-08-18 13:06:40 +0200

    First submission
    Added the code for the makeCacheMatrix and the solveCache

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Code for the R course R Programming Week 3
+## The functions are allow us to generate a special type of matrix that has the capability to store its inverse
 
-## Write a short comment describing this function
+## Creates a special object that allows to store a cache a version of its inverse
+## that is useful to simplify calculations where the inverse might be needed over and over again
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This functions calculates the inverse of a matrix for the special matrix type "makeCacheMatrix"
+## It will try to use a cached result before attemping to re-calculate the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached inverse matrix")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+  ## Return a matrix that is the inverse of 'x'
 }
+
+# Test 1: Generate square matrix
+x <- matrix(1:4, 2, 2)
+m <- makeCacheMatrix(x)
+# Check that the matrix is stored correctly
+m$get()
+# Solve once using solver
+cacheSolve(m)
+# Retrieve once from cache
+cacheSolve(m)
